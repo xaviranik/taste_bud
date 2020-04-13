b94adada0bf7685b0ff61f7492118f308dd5e7c6
xavir <xaviranik.me@gmail.com>
2020-04-13 13:24:44 +0600

    Added: Restaurant menu UI

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -1,18 +1,104 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:taste_bud/data/data.dart';
+import 'package:taste_bud/models/food.dart';
 import 'package:taste_bud/models/restaurant.dart';
 import 'package:taste_bud/widgets/rating_stars.dart';
 
 class RestaurantScreen extends StatefulWidget {
   final Restaurant restaurant;
 
-  RestaurantScreen({Key key, this.restaurant}) : super(key: key);
+  RestaurantScreen({this.restaurant});
+
   @override
   _RestaurantScreenState createState() => _RestaurantScreenState();
 }
 
 class _RestaurantScreenState extends State<RestaurantScreen> {
+  _buildMenuItem(Food menuItem) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            height: 150.0,
+            width: 150.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(menuItem.imageUrl),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+          ),
+          Container(
+            height: 150.0,
+            width: 150.0,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.black.withOpacity(0.5),
+                  Colors.black87.withOpacity(0.5),
+                  Colors.black54.withOpacity(0.5),
+                  Colors.black38.withOpacity(0.3),
+                ],
+                stops: [0.1, 0.4, 0.6, 0.9],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 65.0,
+            child: Column(
+              children: <Widget>[
+                Text(
+                  menuItem.name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(height: 8.0,),
+                Text(
+                  '\$${menuItem.price}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 10.0,
+            right: 10.0,
+            child: Container(
+              height: 30.0,
+              width: 30.0,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(50.0),
+              ),
+              child: IconButton(
+                icon: Icon(Icons.add),
+                iconSize: 15.0,
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,14 +109,14 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
               Hero(
                 tag: widget.restaurant.imageUrl,
                 child: Image(
-                  height: 220.0,
+                  height: 180.0,
                   width: MediaQuery.of(context).size.width,
                   image: AssetImage(widget.restaurant.imageUrl),
                   fit: BoxFit.cover,
                 ),
               ),
               Padding(
-                padding: EdgeInsets.symmetric(vertical: 16.0),
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -45,110 +131,103 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
                       color: Theme.of(context).primaryColor,
                       iconSize: 24.0,
                       onPressed: () {},
-                    )
+                    ),
                   ],
                 ),
               ),
             ],
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          child: Text(
-                            widget.restaurant.name,
-                            style: TextStyle(
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 1.2,
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Chip(
-                          backgroundColor: Colors.green[400],
-                          label: Text(
-                            'OPEN',
-                            style: TextStyle(
-                                fontSize: 14.0,
-                                color: Colors.white
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 8.0,),
-                    RatingStars(rating: widget.restaurant.rating,),
-                    SizedBox(height: 16.0,),
                     Text(
-                      widget.restaurant.address,
+                      widget.restaurant.name,
                       style: TextStyle(
-                        fontSize: 14.0,
-                        color: Colors.grey[600]
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.2,
                       ),
                     ),
-                    SizedBox(height: 8.0,),
-                    Text(
-                      '0.2 miles away',
-                      style: TextStyle(
-                          fontSize: 14.0,
-                          color: Colors.grey[600]
-                      ),
-                    ),
-                    SizedBox(height: 16.0,),
                     Row(
                       children: <Widget>[
-                        FlatButton(
-                          onPressed: () {},
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0)
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 16.0),
-                          color: Theme.of(context).primaryColor,
-                          child: Text(
-                            'Reviews',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold
-                            ),
+                        Text(
+                          '0.2 miles away',
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.grey[600],
                           ),
                         ),
-                        SizedBox(width: 16.0,),
-                        FlatButton(
-                          onPressed: () {},
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0)
-                          ),
-                          padding: EdgeInsets.symmetric(horizontal: 16.0),
-                          color: Theme.of(context).primaryColor,
-                          child: Text(
-                            'Customer Service',
-                            style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold
-                            ),
+                        SizedBox(width: 8.0,),
+                        Text(
+                          'Open',
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold
                           ),
                         ),
                       ],
                     ),
                   ],
-                )
-              ),
-            ],
+                ),
+                SizedBox(height: 8.0),
+                RatingStars(rating: widget.restaurant.rating),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      widget.restaurant.address,
+                      style: TextStyle(fontSize: 12.0, color: Colors.grey[600],),
+                    ),
+                    FlatButton(
+                      padding: EdgeInsets.symmetric(horizontal: 4.0,),
+                      color: Theme.of(context).primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Text(
+                        'Reviews',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
+          SizedBox(height: 16.0),
+          Text(
+            'Menu'.toUpperCase(),
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.2,
+            ),
+          ),
+          SizedBox(height: 10.0),
+          Expanded(
+            child: GridView.count(
+              padding: EdgeInsets.all(10.0),
+              crossAxisCount: 2,
+              children: List.generate(widget.restaurant.menu.length, (index) {
+                Food food = widget.restaurant.menu[index];
+                return _buildMenuItem(food);
+              }),
+            ),
+          )
         ],
       ),
     );
   }
 }
-
